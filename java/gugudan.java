5ca43ae5c79e0bb9dacbd2a9ef20f5c55dc4b1dc
jioh <ajy2277@gmail.com>
2025-03-13 06:21:19 +0000

    java

---
+++ java/gugudan.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class gugudan {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        do {
+            System.out.print("Insert Numver(0:Exit) : ");
+            int i = s.nextInt();
+
+            if(i==0){
+                System.exit(1);
+            }
+        }
+    }
+}
