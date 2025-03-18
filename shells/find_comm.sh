e191f7d758fdd9374f83a2f605558e3a9cda5af6
jioh <ajy2277@gmail.com>
2025-03-18 07:41:04 +0000

    a

---
+++ shells/find_comm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+dir1=$1
+dir2=$2
+
+( cd $dir1; find . -maxdepth 1 -type f -print | sort ) > dir1-file.lst
+( cd $dir2; find . -maxdepth 1 -type f -print | sort ) > dir2-file.lst
+
+comm dirr1-file.lst dir2-file.lst
+
+
