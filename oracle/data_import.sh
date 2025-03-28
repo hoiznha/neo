55585e6a521cd4b2135993a50ed6accb5da89dd7
hoiznha <ajy2277@naver.com>
2025-03-28 14:08:05 +0900

    oracle_node_post

--- oracle/data_import.sh
+++ oracle/data_import.sh
@@ -7,7 +7,7 @@ sqlplus system/1234@xe < shop_membertbl.sql
 sqlplus system/1234@xe < shop_producttbl.sql
 sqlplus system/1234@xe < shop_usertbl.sql
 sqlplus system/1234@xe < shop_buytbl.sql
-
+sqlplus system/1234@xe < hr_usertbl.sql
 sqlplus system/1234@xe < hr_buytbl.sql
 echo
 echo "Database Data Import Done !!"  
--- oracle/node/web.js
+++ oracle/node/web.js
@@ -3,64 +3,109 @@ var dbConfig = require('./dbConfig');
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-const { queryObjects } = require('v8');
 
-oracledb.initOracleClient({libDir:'/opt/oracle/instantclient_21_13'})
+oracledb.initOracleClient({ libDir: '/opt/oracle/instantclient_21_13' })
 
 var app = express();
-app.set('Port',process.env.PORT || 3000);
-app.set(bodyParser.urlencoded({extenede:true}));
-app.set(bodyParser.json());
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 oracledb.autoCommit = true;
 
-app.get('/', function(req,res){
-    res.send('Web Server Started~!!');
+app.get('/', (req, res) => {
+    res.send('Web Server started~!!');
 })
 
-app.get('/dbTestSelect', function(req,res){
+app.get('/dbTestSelect', function (req, res) {
     oracledb.getConnection(
         {
-            user:dbConfig.user,
+            user: dbConfig.user,
             password: dbConfig.password,
             connectString: dbConfig.connectString
-        }, 
-        function(err, connection){
-            if(err) {
+        },
+        function (err, connection) {
+            if (err) {
                 console.error(err.message);
                 return;
             }
-            let qurey = 'select * from usertbl';
+            let query = "select * from usertbl";
 
-            connection.execute(query, [],
-                function (err,result){
-                    if(err){
-                        console.error(err.message);
-                        doRelease(connection);
-                        return;
-                    }
-                    console.log(result.metaData);
-                    console.log(result.rows);
-                })
+            connection.execute(query, [], function (err, result) {
+                if (err) {
+                    console.err(err.message);
+                    doRelease(connection);
+                    return;
+                }
+                console.log(result.rows);
+                doRelease(connection, result.rows);
             })
-            function doRelease(connection,rowList) {
-                connection.release(
-                    function (err) {
-                        if(err) {
-                            console.error(err.message);
-                        }
-                        console.log('List size :' + rowList.length);
-                        res.send(rowList);
-                    }
-                )
+        })
+
+    function doRelease(connection, rowList) {
+        connection.release(
+            function (err) {
+                if (err) {
+                    console.error(err.message);
+                }
+                console.log('List size : ' + rowList.length);
+                res.send(rowList);
             }
-})        
+        )
+    }
+});
 
+app.post('/dbTestInsert', function (req, res) {
+    oracledb.getConnection({
+        user: dbConfig.user,
+        password: dbConfig.password,
+        connectString: dbConfig.connectString
+    },
+        function (err, connection) {
+            if (err) {
+                console.error(err.message);
+                return;
+            }
+            let query = 'insert into usertbl(userid, username, birthyear, addr, mobile1, mobile2, height, mdate) ' +
+                'values(:userid, :username, :birthyear, :addr, :mobile1, :mobile2, :height, :mdate)';
 
-app.all('*', function(req, res) {
-    res.status(404).send('<h1>404 Not Found</h1>');
+            let binddata = [
+                req.body.userid,
+                req.body.username,
+                Number(req.body.birthyear),
+                req.body.addr,
+                req.body.mobile1,
+                req.body.mobile2,
+                Number(req.body.height),
+                new Date(req.body.mdate)
+            ];
+
+            connection.execute(query, binddata, function (err, result) {
+                if (err) {
+                    console.error(err.message);
+                    doRelease(connection);
+                    return;
+                }
+                console.log('Row Insert : ' + result.rowsAffected);
+                doRelease(connection, result.rowsAffected);
+            });
+        });
+
+    function doRelease(connection, result) {
+        connection.release(function (err) {
+            if (err) {
+                console.error(err.message);
+            }
+            res.sendStatus(200);
+        })
+    }
 })
 
-app.listen(app.get('Port'), function() {
-    console.log('3000 Port: Server started~!!')
+app.all('*', function (req, res) {
+    res.status(404).send('<h1>Error - Page is not found.</h1>');
 })
+
+app.listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
+})
+
