8b0d01f3dbdc71b33a838bc92249eab3cf359df3
jioh <ajy2277@gmail.com>
2025-03-28 00:55:53 +0000

    replica_sh_modify

--- mongodb/replica/start_replica.sh
+++ mongodb/replica/start_replica.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-master=$(ps -ef | grep 'master')
-slave1=$(ps -ef | grep 'slave1')
-slave2=$(ps -ef | grep 'slave2')
-arbiter=$(ps -ef | grep 'arbiter')
+/replica/mongodb/bin/mongod --config /work/neo/mongodb/replica/master.conf &
+/replica/mongodb/bin/mongod --config /work/neo/mongodb/replica/slave1.conf &
+/replica/mongodb/bin/mongod --config /work/neo/mongodb/replica/slave2.conf &
+/replica/mongodb/bin/mongod --config /work/neo/mongodb/replica/arbiter.conf &
+sleep 2s
 
-second1=$(echo ${master} | cut -d " " -f2)
-second2=$(echo ${slave1} | cut -d " " -f2)
-second3=$(echo ${slave2} | cut -d " " -f2)
-second4=$(echo ${arbiter} | cut -d " " -f2)
-
-for var in $second1 $second2 $second3 $second4
-do
-	echo $var
-	if [ -n ${var} ]; then
-		result=$(kill -9 ${var})
-		echo process is killed.
-	else
-		echo running process not found.
-	fi
-done	
-echo
+ps -ef | grep mongo
+sleep 3s
+netstat -ntlp | grep mongo
