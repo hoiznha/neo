1ce6fb2ea0a720751c28059ca9de626d8bb96d12
jioh <ajy2277@gmail.com>
2025-03-31 01:48:54 +0000

    mysql_pool

--- node/mysql2-web/app.js
+++ node/mysql2-web/app.js
@@ -1,55 +1,20 @@
-var mysql = require('mysql2/promise');
-const env = require('dotenv').config({ path: "../../.env" });
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-const db = async () => {
-      try {
-        let connection = await mysql.createConnection({
-        host : process.env.host,
-        user: process.env.user,
-        port: process.env.port,
-        password: process.env.password,
-        database: process.env.database
-      })
+app.set('port', process.env.Port || 8000);
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-    let [rows,fields] = await connection.query('select * from st_info');
-    console.log(rows);
-    
+var main = require('./routes/main');
+app.use('/', main);
 
-    let data = {
-        st_id:"202599",
-        name: "Moon",
-        dept:"Computer"
-    };
-
-    let insertId = data.st_id;
-
-     //insert query
-     [rows,fields] = await connection.query('insert into st_info values ? ',data);
-     console.log("Data is inserted : " + insertId);
-
-     //select * query for inserted data
-     [rows,fields] = await connection.query('select * from st_info where ST_ID = ?',insertId);
-     console.log(rows);
-
-     //update query
-     [rows,fields] = await connection.query('update st_info set dept = ?,where ST_ID = ?', ["Game",insertId]);
-     console.log("Data is updated : " + insertId);
-
-     //select * query for updated data
-     [rows,fields] = await connection.query('select * from st_info where ST_ID = ?',insertId);
-     console.log(rows);
-
-     //delete query
-     [rows,fields] = await connection.query('delete from st_info where ST_ID = ?',insertId);
-     console.log("Data is deleted : " + insertId);
-
-     //select * query for deleted data
-     [rows,fields] = await connection.query('select * from st_info where ST_ID = ?',insertId);
-     console.log(rows);
-    } catch(error){
-        console.log(error);
-    }
-}
-
-db();
-
+app.listen(app.get('port'), function () {
+    console.log("Server is Started~!!" + app.get('port'));
+});
