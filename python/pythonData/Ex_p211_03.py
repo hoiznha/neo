19aa0489bb8ad410fc5f94df1847b9cae976f8f9
jioh <ajy2277@gmail.com>
2025-04-11 06:52:42 +0000

    pandas_df

---
+++ python/pythonData/Ex_p211_03.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from pandas import Series
+
+filename = 'fruits.csv'
+df = pd.read_csv(filename, index_col='과일명',encoding='utf-8')
+print('\n원본 데이터 프레임')
+print(df)
+print('-'*50)
+
+print('\n 구입액과 수입량의 소계')
+print(df.sum(axis=0))
+print('-'*50)
+
+print('\n 과일별 소계')
+print(df.sum(axis=1))
+print('-'*50)
+
+print('\n 구입액과 수입량의 평균')
+print(df.round(df.mean(axis=0),2))
+print('-'*50)
+
+print('\n 과일별 평균')
+print(df.round(df.mean(axis=1),2))
+print('-'*50)
