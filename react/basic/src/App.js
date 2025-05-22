c9336c2b8783308789100a0b6677951b1dff988a
hoiznha <ajy2277@naver.com>
2025-05-22 06:14:25 +0000

    react

---
+++ react/basic/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Hello from "./Hello";
+import Wrapper from "./Wrapper";
+import Counter from './Counter';
+
+function App(){
+  return <Counter />;
+}
+
+export default App;
