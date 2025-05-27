aa5d7599f5481f138617873e1149ab6cf239db9d
hoiznha <ajy2277@naver.com>
2025-05-27 02:27:54 +0000

    openai langchain

---
+++ ai-1/3_langchain/p214_app.py
@@ -0,0 +1,26 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage   
+)
+
+chat_llm = ChatOpenAI(
+    model='gpt-3.5-turbo',
+    temperature=0,
+)
+
+message = [
+    HumanMessage(content="고양이의 울음소리는?")
+]
+result = chat_llm(message)
+print('-' * 50)
+print(result)
+
+prompts = ["고양이의 울음소리는?", "강아지의 울음소리는?"]
+
+for i, prompt in enumerate(prompts):
+    messages = [HumanMessage(content=prompt)]
+    result = chat_llm(messages)
+    print('-' * 50)
+    print(f"result {i} : {result.content}" )
